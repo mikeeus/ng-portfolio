1af913fbf830071f712e805e0fd9c765400da2dd
mikeeus <itsmikias@gmail.com>
2018-07-03 16:11:33 +0300

    Create api.service

---
+++ libs/api/src/lib/api.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Post } from '@portfolio/blog';
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs/Observable';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(private http: HttpClient) { }
+
+  getPosts(): Observable<Post[]> {
+    return this.http.get<Post[]>("/api/posts").pipe(
+      map(posts => posts.map(p => new Post(p)))
+    )
+  }
+}
