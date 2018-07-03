73941d6382904dca09dbff7b486e08fafa097553
mikeeus <itsmikias@gmail.com>
2018-07-03 17:26:56 +0300

    Display posts as json and add styling

--- libs/blog/src/lib/containers/posts/posts.component.ts
+++ libs/blog/src/lib/containers/posts/posts.component.ts
@@ -1,20 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Store } from '@ngrx/store';
+import * as fromFeature from '../../+state';
+import { Post } from '../../models';
+import { Observable } from 'rxjs/Observable';
+
 @Component({
   selector: 'portfolio-blog-posts',
   template: `
     <div class="container">
       <h1>Latest Posts</h1>
-      ...
+      <div class="posts">
+        {{ posts | async | json }}
+      </div>
     </div>
   `,
   styleUrls: ['./posts.component.scss']
 })
 export class PostsComponent implements OnInit {
+  posts: Observable<{data: Post[], loading: boolean, loaded: boolean}>;
 
-  constructor() { }
+  constructor(private store: Store<fromFeature.BlogState>) {}
 
   ngOnInit() {
+    this.posts = this.store.select(fromFeature.getPosts);
   }
 
 }
