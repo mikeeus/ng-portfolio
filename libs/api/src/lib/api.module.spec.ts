b1af193b36d08023bc7b033886d026ea663b89de
mikeeus <itsmikias@gmail.com>
2018-07-03 16:01:47 +0300

    Generate api library

---
+++ libs/api/src/lib/api.module.spec.ts
@@ -0,0 +1,16 @@
+import { async, TestBed } from '@angular/core/testing';
+import { ApiModule } from './api.module';
+
+describe('ApiModule', () => {
+  beforeEach(
+    async(() => {
+      TestBed.configureTestingModule({
+        imports: [ApiModule]
+      }).compileComponents();
+    })
+  );
+
+  it('should create', () => {
+    expect(ApiModule).toBeDefined();
+  });
+});
