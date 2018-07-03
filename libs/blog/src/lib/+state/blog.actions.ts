9b2ebcc0ff45b05330399d5d08586d90868a429a
mikeeus <itsmikias@gmail.com>
2018-07-03 15:58:30 +0300

    Add load posts actions

--- libs/blog/src/lib/+state/blog.actions.ts
+++ libs/blog/src/lib/+state/blog.actions.ts
@@ -1,9 +1,14 @@
 import { Action } from '@ngrx/store';
+import { Post } from '../models';
 
 export enum BlogActionTypes {
   BlogAction = '[Blog] Action',
   LoadBlog = '[Blog] Load Data',
-  BlogLoaded = '[Blog] Data Loaded'
+  BlogLoaded = '[Blog] Data Loaded',
+
+  LoadPosts = '[Blog] Load Posts',
+  LoadPostsSuccess = '[Blog] Load Posts Success',
+  LoadPostsFail = '[Blog] Load Posts Fail'
 }
 
 export class Blog implements Action {
@@ -19,4 +24,21 @@ export class BlogLoaded implements Action {
   constructor(public payload: any) {}
 }
 
-export type BlogActions = Blog | LoadBlog | BlogLoaded;
+export class LoadPosts implements Action {
+  readonly type = BlogActionTypes.LoadPosts;
+}
+export class LoadPostsSuccess implements Action {
+  readonly type = BlogActionTypes.LoadPostsSuccess;
+  constructor(public payload: Post[]){}
+}
+export class LoadPostsFail implements Action {
+  readonly type = BlogActionTypes.LoadPostsFail;
+}
+
+export type BlogActions = Blog
+  | LoadBlog
+  | BlogLoaded
+  
+  | LoadPosts
+  | LoadPostsSuccess
+  | LoadPostsFail;
