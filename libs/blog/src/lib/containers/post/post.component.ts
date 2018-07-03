b13e79f3815ea4fda6a2e70d0638512190c9cd51
mikeeus <itsmikias@gmail.com>
2018-07-03 19:16:14 +0300

    Get viewed post and display as json

--- libs/blog/src/lib/containers/post/post.component.ts
+++ libs/blog/src/lib/containers/post/post.component.ts
@@ -1,19 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 
+import * as fromFeature from '../../+state';
+
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs/Observable';
+import { Post } from '@portfolio/models';
+
 @Component({
   selector: 'portfolio-post',
   template: `
-    <p>
-      post works!
-    </p>
+    <div class="container">
+      <div class="post" *ngIf="post$ | async as post">
+        {{ post | json }}
+      </div>
+    </div>
   `,
   styleUrls: ['./post.component.scss']
 })
 export class PostComponent implements OnInit {
-
-  constructor() { }
+  post$: Observable<{ data: Post, loading: boolean, loaded: boolean }>;
+  constructor(private store: Store<any>) { }
 
   ngOnInit() {
+    this.post$ = this.store.select(fromFeature.getViewedPost);
   }
 
 }
